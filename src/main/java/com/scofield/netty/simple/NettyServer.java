ff586e7b44b195d6ef06e785bac2e4efbe89990c
scofield <liyunan@sinosoft.com.cn>
2020-12-24 15:32:14 +0800

    Future-Listener机制，异步执行，增大吞吐量

--- src/main/java/com/scofield/netty/simple/NettyServer.java
+++ src/main/java/com/scofield/netty/simple/NettyServer.java
@@ -1,10 +1,7 @@
 package com.scofield.netty.simple;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -33,9 +30,19 @@ public class NettyServer {
                     });
             //绑定一个端口并且同步，生成一个ChannelFuture对象
             //启动服务器
-            ChannelFuture sync = bootstrap.bind(6668).sync();
+            ChannelFuture cf = bootstrap.bind(6668).sync();
+            cf.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (cf.isSuccess()) {
+                        System.out.println("监听端口6668成功");
+                    } else {
+                        System.out.println("监听6668失败");
+                    }
+                }
+            });
             //对关闭通道进行监听
-            sync.channel().closeFuture().sync();
+            cf.channel().closeFuture().sync();
         } finally {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
