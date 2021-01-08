ec9622f807d6edcec5aa62ab3f2274d9396b1e8d
hegf <zkrhegf@abchinalife.cn>
2021-01-08 16:30:37 +0800

    protobuf传输数据

---
+++ src/main/java/com/scofield/netty/codec/NettyServer.java
@@ -0,0 +1,51 @@
+package com.scofield.netty.codec;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+    public static void main(String[] args) throws InterruptedException {
+        //创建两个线程组bossGroup和workerGroup
+        //bossGroup只是处理连接请求，真正和客户端业务处理的会交给workerGroup完成
+        //两个都是无限循环
+        //bossGroup 和 workerGroup含有的子线程（NioEventLoop）的个数默认是CPU核数 * 2
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            //创建服务器的启动对象，配置参数
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)//设置两个线程组
+                    .channel(NioServerSocketChannel.class)//使用NioSocketChannel作为服务器的通道实现
+                    .option(ChannelOption.SO_BACKLOG, 128)//设置线程队列链接个数
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)//设置保持活动链接状态
+                    .childHandler(new ChannelInitializer<SocketChannel>() {//创建一个通道测试对象
+                        //给pipeline设置处理器
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new NettyServerHandler());
+                        }
+                    });
+            //绑定一个端口并且同步，生成一个ChannelFuture对象
+            //启动服务器
+            ChannelFuture cf = bootstrap.bind(6668).sync();
+            cf.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (cf.isSuccess()) {
+                        System.out.println("监听端口6668成功");
+                    } else {
+                        System.out.println("监听6668失败");
+                    }
+                }
+            });
+            //对关闭通道进行监听
+            cf.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/scofield/netty/codec/NettyServerHandler.java
@@ -0,0 +1,64 @@
+package com.scofield.netty.codec;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+import java.util.concurrent.TimeUnit;
+
+//我们自定义一个Handler需要继承netty规定好的某个HandlerAdapter（规范）
+//这是我们自定义一个Handler才能成为一个Handler
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+    //读取数据的事件（这里我们可以读取客户端发送的消息）
+    //ChannelHandlerContext：上下文对象，含有pipeline， channel，地址
+    //Object msg:就是客户端发送的数据
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //假如这里我们有一个耗时长的业务，我们应该异步执行，也就是将该业务提交该channel对应的NIOEventLoop的taskQueue中
+        //第一种方法，自定义普通任务
+        //该方式会将该线程提交到taskQueue中，然后异步执行
+        ctx.channel().eventLoop().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10 * 1000);
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("hello，客户端2", CharsetUtil.UTF_8));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        //第二种方法，用户自定义定时任务，该任务会将该线程提交到scheduleTaskQueue中
+        ctx.channel().eventLoop().schedule(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10 * 1000);
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("hello，客户端2", CharsetUtil.UTF_8));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 5, TimeUnit.SECONDS);
+        /*System.out.println("服务器："+ctx);
+        //ByteBuf是netty提供的
+        ByteBuf byteBuffer = (ByteBuf) msg;
+        System.out.println("客户端发送的数据："+byteBuffer.toString(CharsetUtil.UTF_8));
+        System.out.println("客服端地址："+ctx.channel().remoteAddress());*/
+    }
+
+    //数据读取完毕
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        //writeAndFlush是write并且flush，将数据写入缓存并刷新
+        //一般讲，我们要对发送的数据进行编码
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello，客户端", CharsetUtil.UTF_8));
+    }
+
+    //处理异常，一般需要关闭通道
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
